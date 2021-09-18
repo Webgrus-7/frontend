e23de051242d1d303566bc8cef2343ded1d5a741
qhdud <index_1234@naver.com>
2021-09-18 19:30:58 +0900

    메인 화면 퍼블리싱 4차

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Header from "./header";
 import "./css/main.css";
 import ddaom from "./img/ddaom1.png";
+import arrow from "./img/arrow.png";
 function Main() {
     let text_01 = ["나만의 공부메이트, 웹 테스트와 함께해요.",<br/>,
         "웹테스트인과 차근차근 함께 성장할 수 있어요."]
@@ -24,19 +25,33 @@ function Main() {
             </div>
             <div className="main-02">
                 <span className="main__hash">#</span>
-                <div className="main__dot"></div>
-                <div className="main__dot"></div>
+                <span className="main__dot-01">·</span>
+                <span className="main__dot-02">·</span>
                 <span className="main__text-04">영어부터</span>
-                <div className="main__text__block-03">
-                <span className="main__text-04">전공까지</span>
-                </div>
+                <span className="main__text-05">전공까지</span>
                 <div className="main__text__block-04">
-                <span className="main__text-05">{text_02}</span>
+                <span className="main__text-06">{text_02}</span>
                 </div>
                 <div className="main__button">
                     <span className="main__button__text">더 찾아보기</span>
                 </div>
-                
+                <div className="circle-01">
+                    <span className="circle__text__number-01">1</span>
+                    <span className="circle__text__subject">토익</span>
+                </div>
+                <div className="circle-02">
+                    <span className="circle__text__number-02">2</span>
+                    <span className="circle__text__subject">토플</span>
+                </div>
+                <div className="circle-03">
+                    <span className="circle__text__number-03">3</span>
+                    <span className="circle__text__subject">교양</span>
+                </div>
+                <div className="circle-04">
+                    <span className="circle__text__number-04">4</span>
+                    <span className="circle__text__subject">전공</span>
+                </div>
+                <img className="arrow" src={arrow}></img>
             </div>
         </div>
         
