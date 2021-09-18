d9a06bf88ad74faf3846904b65c06b787aa1cc04
qhdud <index_1234@naver.com>
2021-09-18 20:38:17 +0900

    로그인 화면 퍼블리싱 1차

---
+++ src/login.js
@@ -0,0 +1,18 @@
+import Header from "./header";
+import "./css/login.css";
+
+function Login()
+{
+    return (
+        <div className="login__outer">
+            <Header />
+            <span className="feed__text-01">피드</span>
+            <span className="feed__text-02">Q</span>
+            <div className="login__bar"></div>
+            <div className="login__inner">
+                
+            </div>
+        </div>
+    );
+}
+export default Login
