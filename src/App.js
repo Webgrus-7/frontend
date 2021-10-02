b327529f18cd4332c56d3ffa3558d7b4b1a8d4fd
yerin Jo <76177687+zunjune@users.noreply.github.com>
2021-10-02 14:58:21 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./header";
+import Header2 from "./header2";
+import Main2 from "./main2";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navi">
+        <Header />
+        <Header2 />
+      </div>
+      <Main2 />
+    </div>
+  );
+}
+
+export default App;
